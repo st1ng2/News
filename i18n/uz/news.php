2a6f3f31a56403f1b37ddc59a41f13363e489a7a
Bahodir <st1ng2@protonmail.ch>
2024-06-03 19:50:12 +0500

    update: Uzbek translations

--- i18n/uz/news.php
+++ i18n/uz/news.php
@@ -7,7 +7,16 @@ return [
     'not_found' => 'Yangilik topilmadi',
     'new_title' => 'Yangiliklar - :name',
     'title' => 'Yangiliklar sahifasi',
+    'perm_desc' => 'Flute-da yangiliklarni ko‘rish/tahrirlash uchun ruxsat beradi',
+    'published' => "Nashr qilingan",
+    'will_published' => "Mavjud bo‘ladi: :date",
+    'was_published' => "Nashr qilingan",
+    'views' => 'Ko‘rish(lar)',
+    'stats' => [
+        "title" => "Yangiliklar statistikasi",
+    ],
     'admin' => [
+        'short_desc' => 'Barcha yangiliklar ro‘yxati',
         'title' => 'Yangiliklar',
         'setting' => 'Yangiliklarni sozlash',
         'setting_description' => 'Ushbu sahifada siz yangiliklarni ko‘rishingiz va o‘chirishingiz mumkin',
@@ -21,6 +30,9 @@ return [
         'description_placeholder' => 'Oq choynakka oq qopqoq, ko‘k choynakka ko‘k...',
         'content' => 'Yangilik mazmuni',
         'edit_title' => 'Yangiliklarni o‘zgartirish',
-        'edit_description' => 'Ushbu sahifada siz yangiliklarni o‘zgartirishingiz mumkin'
+        'edit_description' => 'Ushbu sahifada siz yangiliklarni o‘zgartirishingiz mumkin',
+        'published_at' => "Nashr vaqti",
+        "published_at_desc" => "Yangilikni rejalashtirmoqchi bo‘lsangiz, nashr sanasini tanlang",
+        "new_is_not_published" => "Bu yangilik rejalashtirildi va <b>:date</b> da nashr etiladi"
     ]
 ];
